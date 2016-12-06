3ddc31646e809f98fdbc92e27abb41f22b29088d
Harald von Waldow <harald.vonwaldow@eawag.ch>
2016-12-06 17:04:07 +0100

    added gui

--- sav2csv/sav2csv.py
+++ sav2csv/sav2csv.py
@@ -1,8 +1,10 @@
 from savReaderWriter import SavReader
-from os import path
+import os.path
 import sys
 import csv
 import argparse
+from Tkinter import Tk
+import tkFileDialog
 
 
 class RFC4180(csv.Dialect):
@@ -20,17 +22,22 @@ class RFC4180(csv.Dialect):
 def parseargs():
     pa = argparse.ArgumentParser(
         description='Converts an spss "sav" data file to CSV')
-    pa.add_argument('file', metavar='SAV_FILE')
+    pa.add_argument('-f', metavar='SAV_FILE',
+                    help='The .sav file to export. ' +
+                    'If omitted, a graphical file chooser will be used.')
+    pa.add_argument('-o', metavar='OUTPUTDIRECTORY',
+                    help='The output directory. Default is the current ' +
+                    'directory if SAV_FILE was given, otherwise a ' +
+                    'file chooser will be used as well.')
     args = pa.parse_args(sys.argv[1:])
     return vars(args)
 
 
 def _stringify(dat):
-    if isinstance(dat, basestring):
-        return dat.encode('utf-8')
+    if not isinstance(dat, basestring):
+        return str(dat).encode('utf-8')
     else:
-        return dat
-
+        return dat.encode('utf-8')
 
 def convert(infile, outfile):
     with SavReader(infile, returnHeader=True, ioUtf8=True,
@@ -41,10 +48,36 @@ def convert(infile, outfile):
                 writer = csv.writer(fout, dialect='RFC4180')
                 writer.writerow(l)
 
+def get_paths():
+    home = os.path.expanduser('~')
+    infile = parseargs()['f']
+    out_dir = parseargs()['o']
+    if infile is None:
+        root = Tk()
+        root.withdraw()
+        f = tkFileDialog.askopenfile(title='Choose file to convert',
+                                      filetypes=[('sav', '*.sav')],
+                                      initialdir=home)
+        if f:
+            infile = f.name
+            f.close()
+        else:
+            sys.exit()
+        if out_dir is None:
+            out_dir = tkFileDialog.askdirectory(title='Choose output directory',
+                                                initialdir=home)
+            if not out_dir:
+                sys.exit()
+        root.destroy()
+    if not out_dir:
+        out_dir = os.getcwd()
+    return (infile, out_dir)
+
 def main():
     csv.register_dialect(u'RFC4180', RFC4180)
-    infile = parseargs()['file']
-    outfile = path.splitext(infile)[0] + '.csv'
+    infile, out_dir = get_paths()
+    outfilename = os.path.splitext(os.path.basename(infile))[0] + '.csv'
+    outfile = os.path.join(out_dir, outfilename)
     convert(infile, outfile)
 
 if __name__ == '__main__':
